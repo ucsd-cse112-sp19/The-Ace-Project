4e97c0a3adbb49cb943f7d414dc517745018aca3
Evan Laufer <evan.m.laufer@gmail.com>
2019-04-24 15:23:17 -0700

    Add JSDoc dependency. TODO: Travis Integration

--- temp.js
+++ temp.js
@@ -1,6 +1,9 @@
 /* Dummy JS File so ESLint can run */
 console.log('Hello, World!');
 
+/**
+ * Test function. Returns 'Hello'.
+ */
 function getHelloString() { // eslint-disable-line no-unused-vars
   return 'Hello';
 }
