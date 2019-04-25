a391d2d7c0b0aded2cc032e7de5b50d71958a397
Evan Laufer <evan.m.laufer@gmail.com>
2019-04-24 19:51:23 -0700

    Modifying deploy script for testing...

--- .utils/deploy-docs.sh
+++ .utils/deploy-docs.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 # double check deploy only happens on correct branch
-if [[ "$TRAVIS_REPO_SLUG" == "ucsd-cse112/The-Ace-Project" ]] && [[ "$TRAVIS_PULL_REQUESTS" == "false" ]] && [[ "$TRAVIS_REPO_BRANCH" == "master" ]]; then
+echo "------ Deploying Docs ------"
+if [[ "$TRAVIS_REPO_SLUG" == "ucsd-cse112/The-Ace-Project" ]] && [[ "$TRAVIS_PULL_REQUESTS" == "false" ]]; then
+    echo "Cloning gh-pages branch..."
     cd $HOME
     git config --global user.email "travis@travis-ci.com"
     git config --global user.name "travis-ci"
-    git clone --quiet --branch=gh-pages https://${GITHUB_KEY}@github.com/ucsd-cse112/The-Ace-Project.git webpage > /dev/null 2>&1
+    git clone --branch=gh-pages https://${GITHUB_KEY}@github.com/ucsd-cse112/The-Ace-Project.git webpage > /dev/null 2>&1
 
+    echo "Moving generated docs..."
     cd webpage
     git rm ./doc
     cp -r $HOME/doc ./doc
+
+    echo "Pushing to gh-pages branch..."
     git add .
     git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
     git push origin gh-pages > /dev/null
