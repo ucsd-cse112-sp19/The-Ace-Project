0a2988f460bed8e995fade79808c766e9c40f90d
Evan Laufer <evan.m.laufer@gmail.com>
2019-04-29 02:36:52 -0700

    Add auto deploy to github releases.
    Auto deploy merges to master to github releases.

---
+++ .utils/deploy-github.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# A simple bash script to copy release files into a zip to release on github
+# Usage: ./deploy-github.sh
+
+rm -rf $RELEASE_NAME || exit 0
+for jsfile in `find ./components -name "*.min.js"`; do
+    mkdir -p $RELEASE_NAME/$jsfile
+    cp -r $jsfile $RELEASE_NAME/$jsfile
+    tar -czf $RELEASE_NAME-$TRAVIS_TAG.tar.gz $RELEASE_NAME
+    zip -r $RELEASE_NAME-$TRAVIS_TAG.zip $RELEASE_NAME
+done
