e81ae2c63bd347bf53a8612ccc9a2e93c494ab5e
Evan Laufer <evan.m.laufer@gmail.com>
2019-04-29 02:57:30 -0700

    Fix deploy zip name

--- .utils/deploy-github.sh
+++ .utils/deploy-github.sh
@@ -9,4 +9,5 @@ for jsfile in `find ./components -name "*.min.js"`; do
     cp -r $jsfile $RELEASE_NAME/$jsfile
     tar -czf $RELEASE_NAME-$TRAVIS_TAG.tar.gz $RELEASE_NAME
     zip -r $RELEASE_NAME-$TRAVIS_TAG.zip $RELEASE_NAME
+    echo "Release files created at: $RELEASE_NAME-$TRAVIS_TAG.tar.gz"
 done
