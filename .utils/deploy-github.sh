4fc2980127f2f394f5eddeca3f87adff5887d756
Evan Laufer <evan.m.laufer@gmail.com>
2019-05-16 12:56:53 -0700

    Add automatic NPM deployment on merge with master

--- .utils/deploy-github.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# A simple bash script to copy release files into a zip to release on github
-# Usage: ./deploy-github.sh
-
-# tar and zip files
-tar -czf $RELEASE_NAME-$TRAVIS_TAG.tar.gz ./dist
-zip -r $RELEASE_NAME-$TRAVIS_TAG.zip ./dist
-echo "Release files created at: $RELEASE_NAME-$TRAVIS_TAG.tar.gz"
