df2a8545f7ea1fe4e2a56c084eea8de092f138c3
Evan Laufer <evan.m.laufer@gmail.com>
2019-05-16 11:59:59 -0700

    Add webpack to the deploy process

--- .utils/deploy-github.sh
+++ .utils/deploy-github.sh
@@ -3,11 +3,7 @@
 # A simple bash script to copy release files into a zip to release on github
 # Usage: ./deploy-github.sh
 
-rm -rf $RELEASE_NAME || exit 0
-for jsfile in `find ./components -name "*.min.js"`; do
-    mkdir -p $RELEASE_NAME/$jsfile
-    cp -r $jsfile $RELEASE_NAME/$jsfile
-    tar -czf $RELEASE_NAME-$TRAVIS_TAG.tar.gz $RELEASE_NAME
-    zip -r $RELEASE_NAME-$TRAVIS_TAG.zip $RELEASE_NAME
-    echo "Release files created at: $RELEASE_NAME-$TRAVIS_TAG.tar.gz"
-done
+# tar and zip files
+tar -czf $RELEASE_NAME-$TRAVIS_TAG.tar.gz ./dist
+zip -r $RELEASE_NAME-$TRAVIS_TAG.zip ./dist
+echo "Release files created at: $RELEASE_NAME-$TRAVIS_TAG.tar.gz"
