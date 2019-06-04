a032d182e061fc6de722d3bce564da7a473cd6ac
Evan Laufer <evan.m.laufer@gmail.com>
2019-06-04 14:00:22 -0700

    feat(core-link.js): add boostrap support to core-link
    add bootstrap styling to the core-link css, allowing bootstrap colors

--- components/core-link/core-link.js
+++ components/core-link/core-link.js
@@ -1,4 +1,10 @@
 // @ts-check
+
+// @ts-ignore
+import htmlTemplate from './core-link.html';
+// @ts-ignore
+import cssTemplate from './core-link.css';
+
 /**
  * Text hyperlink
  * @example <core-link> Text </core-link>
@@ -18,60 +24,13 @@ class CoreLink extends HTMLElement {
   constructor() {
     super();
     this.template = document.createElement('template');
-    this.template.innerHTML = `
-      <style>
-        :host(*) {
-          --main-font-size: 14px;
-          --main-font-family: Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,SimSun,sans-serif;
-          font-size: var(--main-font-size);
-          font-family: var(--main-font-family);
-          cursor: pointer;
-          text-decoration: none;
-        }
-
-        :host(:hover:not([disabled]):not([underline="false"])) {
-          text-decoration: underline;
-        }
-        
-        :host([type="primary"]){color:#409eff}
-        :host([type="danger"]){color:#f56c6c}        
-        :host([type="success"]){color:#67c23a}
-        :host([type="warning"]){color:#e6a23c}
-        :host([type="info"]){color:#909399}
-
-        :host(:hover){color:#409eff;}
-        :host(:hover[type="primary"]){color:#66b1ff}
-        :host(:hover[type="danger"]){color:#f78989}
-        :host(:hover[type="success"]){color:#85ce61}
-        :host(:hover[type="warning"]){color:#ebb563}
-        :host(:hover[type="info"]){color:#a6a9ad}
-
-        :host(:after){border-color:#409eff;}
-        :host(:after[type="primary"]){border-color:#409eff}
-        :host(:after[type="danger"]){border-color:#f56c6c}
-        :host(:after[type="success"]){border-color:#67c23a}
-        :host(:after[type="warning"]){border-color:#e6a23c}
-        :host(:after[type="info"]){border-color:#909399}
-
-        :host([disabled]){color:#c0c4cc;}
-        :host([disabled][type="primary"]){color:#a0cfff}
-        :host([disabled][type="danger"]){color:#fab6b6}
-        :host([disabled][type="success"]){color:#b3e19d}
-        :host([disabled][type="warning"]){color:#f3d19e}
-        :host([disabled][type="info"]){color:#c8c9cc}
-
-        :host(:after:hover:not([underline="false"][type="primary"]){border-color:#409eff}
-        :host(:after:hover:not([underline="false"][type="danger"]){border-color:#f56c6c}
-        :host(:after:hover:not([underline="false"][type="success"]){border-color:#67c23a}
-        :host(:after:hover:not([underline="false"][type="warning"]){border-color:#e6a23c}
-        :host(:after:hover:not([underline="false"][type="info"]){border-color:#909399}
-
-        </style>
-        <span><slot/></span>        
-      `;
+    this.template.innerHTML = htmlTemplate;
+    this.styleNode = document.createElement('style');
+    this.styleNode.innerHTML = cssTemplate;
 
     const shadowRoot = this.attachShadow({ mode: 'open' });
     shadowRoot.appendChild(this.template.content.cloneNode(true));
+    shadowRoot.appendChild(this.styleNode);
   }
 
   attributeChangedCallback(attrName, oldVal, newVal) {
