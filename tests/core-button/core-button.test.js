eb028dcf0628e756ec69c485ec1ba82da2c350c3
Hiroto Kakitani <hirotokakitani@gmail.com>
2019-06-12 18:18:50 -0700

    test(core-button): finished core-button integration tests
    refactored core-button tests so linter doesnt complain
    
    3637

--- tests/core-button/core-button.test.js
+++ tests/core-button/core-button.test.js
@@ -3,39 +3,50 @@ import { Selector } from 'testcafe';
 fixture('core-button integration tests')
   .page('./core-button.test.html');
 
-test('Default Button Clicks', async (t) => {
-  const buttonList = Selector('#defaultButtons').child();
-  const count = await buttonList.count;
+async function checkBehavior(button, disabled, t) {
+  // async stuff here
+  const buttonType = await button.textContent;
+  await t.click(button);
+  const expected = disabled ? buttonType : `${buttonType} Clicked!`;
+  await t.expect(button.textContent)
+    .contains(expected);
+}
 
+async function testClicks(containerId, disabled, t) {
+  const buttonList = await Selector(containerId).child();
+  const count = await buttonList.count;
+  const results = [];
   for (let i = 0; i < count; i += 1) {
     const button = buttonList.nth(i);
-    console.log(button);
-    console.log(t);
-    // await t.click(button);
+    results.push(checkBehavior(button, disabled, t));
   }
+  return Promise.all(results);
+}
+
+test('Default Button Clicks', async (t) => {
+  await testClicks('#defaultButtons', false, t);
 });
-/*
+
 test('Disabled Button Clicks', async (t) => {
-  testClicks('#disabledButtons', true, t);
- });
+  await testClicks('#disabledButtons', true, t);
+});
 
 test('Plain Button Clicks', async (t) => {
-  testClicks('#plainButtons', false, t);
+  await testClicks('#plainButtons', false, t);
 });
 
 test('Icon Button Clicks', async (t) => {
-  testClicks('#iconButtons', false, t);
+  await testClicks('#iconButtons', false, t);
 });
 
 test('Loading Button Clicks', async (t) => {
-  testClicks('#loadingButtons', false, t);
+  await testClicks('#loadingButtons', false, t);
 });
 
 test('Round Button Clicks', async (t) => {
-  testClicks('#roundButtons', false, t);
+  await testClicks('#roundButtons', false, t);
 });
 
 test('Circle Button Clicks', async (t) => {
-  testClicks('#circleButtons', false, t);
+  await testClicks('#circleButtons', false, t);
 });
-*/
