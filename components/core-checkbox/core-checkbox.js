a438ed28e00bf2c29312c26e64cd5305978bddc2
vilela69 <frederico.vilelacurti@gmail.com>
2019-06-13 00:41:34 -0700

    fix(checkbox): fixed bugs and visual tweaks

---
+++ components/core-checkbox/core-checkbox.js
@@ -0,0 +1,69 @@
+// @ts-check
+
+// @ts-ignore
+import htmlTemplate from './core-checkbox.html';
+// @ts-ignore
+import cssTemplate from './core-checkbox.css';
+
+/**
+ * A checkbox that can be clicked on.
+ * @example <core-checkbox> Text </core-checkbox>
+ * @property {string} [name="default"] - The name of the button
+ * @property {boolean} [border=false] - Determines if border will be rendered.
+ * @property {boolean} [checked=false] - If present, the checkbox will be checked.
+ * @property {boolean} [disabled=false] - Determines if the checkbox is disabled.
+ * @playground <core-checkbox> Checkbox </core-checkbox>
+ */
+
+class CoreCheckbox extends HTMLElement {
+  static get observedAttributes() {
+    return ['value', 'v-model', 'disabled', 'name', 'checked', 'border'];
+  }
+
+  constructor() {
+    super();
+    this.template = document.createElement('template');
+    this.template.innerHTML = htmlTemplate;
+    this.styleNode = document.createElement('style');
+    this.styleNode.innerHTML = cssTemplate;
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.appendChild(this.template.content.cloneNode(true));
+    shadowRoot.appendChild(this.styleNode);
+
+    this.checkbox = shadowRoot.querySelector('input');
+    this.checked = false;
+  }
+
+  connectedCallback() {
+    this.checked = this.getAttribute('checked') !== null;
+    this.disabled = this.getAttribute('disabled') !== null;
+    this.addEventListener('click', () => {
+      if (!this.disabled) {
+        this.checked = !this.checked;
+        this.setAttribute('checked', this.checked);
+        this.checkbox.checked = this.checked;
+      }
+    });
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    switch (attrName) {
+      case 'disabled':
+        this.shadowRoot.querySelector('input').style.cursor = this.hasAttribute('disabled') ? 'not-allowed' : 'pointer';
+        this.shadowRoot.querySelector('input').disabled = this.hasAttribute('disabled');
+        break;
+      case 'name':
+        this.shadowRoot.querySelector('input').name = newVal;
+        break;
+      case 'checked':
+        this.shadowRoot.querySelector('input').checked = this.hasAttribute('checked');
+        break;
+      case 'border':
+        break;
+      default:
+        this.style.borderRadius = '0px';
+    }
+  }
+}
+
+window.customElements.define('core-checkbox', CoreCheckbox);
--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@
 import CoreHello from './core-hello/core-hello';
 import CoreButton from './core-button/core-button';
 import CoreLink from './core-link/core-link';
+import CoreCheckbox from './core-checkbox/core-checkbox';
